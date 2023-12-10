c8da8c79ea98bde2cb91a4b27bce9e61ae6e5e5b
ahmed <ahmedbassi@gmail.com>
2023-12-10 16:03:31 +1030

    first commit

---
+++ CitationSuggest.ts
@@ -0,0 +1,71 @@
+import {
+	App,
+	Editor,
+	EditorPosition,
+	EditorSuggest,
+	EditorSuggestContext,
+	EditorSuggestTriggerInfo,
+	MarkdownView,
+	TFile,
+} from 'obsidian';
+
+import BibcitePlugin from 'main';
+
+interface Suggestion {
+	query: string;
+	startIndex: number;
+	label: string;
+	isEmptyChoice?: boolean;
+	field?: string;
+}
+
+
+export class CitationSuggest extends EditorSuggest<Suggestion> {
+	private plugin: BibcitePlugin;
+	private readonly app: App;
+	private justCompleted: boolean;
+
+	constructor(app: App, plugin: BibcitePlugin) {
+		super(app);
+		this.app = app;
+		this.plugin = plugin;
+		this.justCompleted = false;
+	}
+	onTrigger(
+		cursor: EditorPosition,
+		editor: Editor,
+		file: TFile
+	): EditorSuggestTriggerInfo | null {
+		if (this.justCompleted) {
+			this.justCompleted = false;
+			return null;
+		}
+
+        const {editor} = this.context;
+
+        const triggerPhrase = '[@';
+        const startPos = this.context?.start || {
+        line: cursor.line,
+        ch: cursor.ch - triggerPhrase.length,
+        };
+
+        return {
+            start: startPos,
+            end: cursor,
+            query: editor.getRange(startPos, cursor).substring(triggerPhrase.length),
+            };
+
+    }
+
+
+	getSuggestions(context: EditorSuggestContext): IDateCompletion[] {
+        const suggestions = this.getDateSuggestions(context);
+        if (suggestions.length) {
+            return suggestions;
+        }
+
+        // catch-all if there are no matches
+        return [{ label: context.query }];
+    }
+
+}
